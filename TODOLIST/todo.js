8b188ca16f5fde4e1c912a3b7c79798ce240b900
BolluHimana <bolluhimana@msitprogram.net>
2020-05-11 10:26:56 +0530

    Done with deleting a todo task!!

--- TODOLIST/todo.js
+++ TODOLIST/todo.js
@@ -1,21 +1,41 @@
-    var myNodelist = document.getElementsByTagName("LI");
-    var i;
-    for (i = 0; i < myNodelist.length; i++) {
-      var s = document.createElement("S");
-      var cont = document.createTextNode("");
-      s.className = "close";
-      s.appendChild(cont);
-      myNodelist[i].appendChild(s);
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var s = document.createElement("S");
+  var cont = document.createTextNode("");
+  s.className = "close";
+  s.appendChild(cont);
+  myNodelist[i].appendChild(s);
+}
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+  var div = this.parentElement;
+  div.style.display = "none";
+  }
+}
+var list = document.querySelector('ul');
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("Task").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+  alert("please enter text!!");
+  } else {
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("Task").value = "";
+  var s = document.createElement("S");
+  var cont = document.createTextNode("");
+  s.className = "close";
+  s.appendChild(cont);
+  li.appendChild(s);
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
     }
-   
-    function newElement() {
-      var li = document.createElement("li");
-      var inputValue = document.getElementById("Task").value;
-      var t = document.createTextNode(inputValue);
-      li.appendChild(t);
-      if (inputValue === '') {
-      alert("please enter text!!");
-      } else {
-        document.getElementById("list").appendChild(li);
-      }
-    }
+  }
+}
